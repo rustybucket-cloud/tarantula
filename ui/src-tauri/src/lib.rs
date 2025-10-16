79fbcf1246b42c639ac02fe218d36ec32addf6a0
Jacob Patton <pattonjacob32@gmail.com>
2025-10-16 13:53:19 -0600

    open card

--- ui/src-tauri/src/lib.rs
+++ ui/src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use shared::app::config;
+use shared::app::run;
 use shared::domain::app::App;
 use shared::infra::app_data;
 
@@ -8,11 +9,17 @@ fn get_app_data() -> Vec<App> {
     app_data::get_apps(&config).unwrap()
 }
 
+#[tauri::command]
+fn run_app(app_name: String) {
+    let config = config::create_config().unwrap();
+    run::run(&app_name, &config).unwrap();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_app_data])
+        .invoke_handler(tauri::generate_handler![get_app_data, run_app])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription, CardAction } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 import "./App.css";
 import { invoke } from "@tauri-apps/api/core";
+import { ExternalLink } from "lucide-react";
 
 type AppType = {
     name: string;
@@ -20,17 +23,38 @@ function App() {
         });
     }, [])
 
+    const handleClick = (app: AppType) => {
+        invoke("run_app", { appName: app.name }).catch((err) => {
+            console.error(err);
+        });
+    }
+
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
-            <ul>
-            {apps.map((app, index) => (
-                <li key={index}>
-                    {app.icon && <img src={app.icon} alt={`${app.name} icon`} width={32} height={32} />}
-                    <a href={app.url} target="_blank" rel="noopener noreferrer">{app.name}</a>
-                </li>
-            ))}
-            </ul>
+      <ul>
+        {apps.map((app, index) => (
+            <li key={index}>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{app.name}</CardTitle>
+                    <CardDescription>{app.url}</CardDescription>
+                    <CardAction>
+                      <Button onClick={() => handleClick(app)}><ExternalLink /></Button>
+                    </CardAction>
+                  </CardHeader>
+                  <CardContent>
+                    <p>Card Content</p>
+                  </CardContent>
+                  <CardFooter>
+                    <p>Card Footer</p>
+                  </CardFooter>
+                </Card>
+                {app.icon && <img src={app.icon} alt={`${app.name} icon`} width={32} height={32} />}
+                <a href={app.url} target="_blank" rel="noopener noreferrer">{app.name}</a>
+            </li>
+        ))}
+    </ul>
     </main>
   );
 }
